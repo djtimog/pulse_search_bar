f352f6356b6d939c9c21b194e35c369541bec020
Christian Ogunleye <117105380+djtimog@users.noreply.github.com>
2024-05-21 15:32:45 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,41 +1,50 @@
-let searchInput = document.querySelector(".search");
-let paragraph = document.querySelector(".para");
-let cleartext = document.querySelector(".cancel");
+window.onload = () => {
+  let searchInput = document.querySelector(".search");
+  let paragraph = document.querySelector(".para");
+  let cleartext = document.querySelector(".cancel");
 
-let s = "";
-
-function search(s) {
-  s += searchInput.value;
-  if (s == "") {
-    alert("There is no content to search...");
-    paragraph.innerHTML = paragraph.textContent;
-    searchInput.value = "";
-    cleartext.innerHTML = ``;
-  } else {
-    changeParagraph(s);
-  }
-}
-
-const changeParagraph = (s) => {
-  s += searchInput.value;
-  paragraph.innerHTML = paragraph.textContent.replace(
-    s,
-    `<span class = "searchedText" id = "searchedText">${s}</span>`
-  );
-  cleartext.innerHTML = `x`;
-};
-
-let a_z = "abcdefghijklmnopqrustuvwxyz!?*,";
-function enter(event) {
-  if (event.key == "Backspace") {
-    search(s);
-  } else if (event.key == "Enter") {
-    search(s);
-  } else {
-    for (let ln of a_z) {
-      if (event.key == ln) {
-        changeParagraph(s);
-      }
+  // changeParagraph();
+  function search(s) {
+    s += searchInput.value;
+    if (s == "") {
+      paragraph.innerHTML = paragraph.textContent;
+      searchInput.value = "";
+      cleartext.innerHTML = ``;
+    } else {
+      changeParagraph(s);
     }
   }
-}
+
+  const changeParagraph = (s) => {
+    paragraph.innerHTML = paragraph.textContent.replace(
+      s,
+      `<span class = "searchedText" id = "searchedText">${s}</span>`
+    );
+    cleartext.innerHTML = `x`;
+  };
+  search("");
+};
+
+setInterval(window.onload, 1000);
+
+// let a_z = "abcdefghijklmnopqrustuvwxyz!?*,";
+// function enter(event) {
+//   if (event.key == "Backspace") {
+//     search();
+//   } else if (event.key == "Enter") {
+//     search(s);
+//   } else {
+//     for (let ln of a_z) {
+//       if (event.key == ln) {
+//         changeParagraph(s);
+//       }
+//     }
+//   }
+// }
+
+// function clearInput(s){
+//   s = "";
+//   searchInput.value = "";
+//   cleartext.innerHTML = ``;
+//   paragraph.innerHTML = paragraph.textContent.replace(`<span class = "searchedText" id = "searchedText">${s}</span>`, s);
+// }
