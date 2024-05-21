87498d1378cff1163583c7abc4a93da5f59663b1
Christian Ogunleye <117105380+djtimog@users.noreply.github.com>
2024-05-21 08:50:07 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,35 +2,40 @@ let searchInput = document.querySelector(".search");
 let paragraph = document.querySelector(".para");
 let cleartext = document.querySelector(".cancel");
 
-
-
 let s = "";
+
 function search(s) {
-    s += searchInput.value;
-    if (s == "") {
-        alert("There is no content to search...");
-        paragraph.innerHTML = paragraph.textContent;
-        searchInput.value = "";
-        cleartext.innerHTML = ``;
-    } else {
-        paragraph.innerHTML = paragraph.textContent.replace(
-        s,
-        `<span class = "searchedText" id = "searchedText">${s}</span>`
-        );
-        cleartext.innerHTML = `x`;
-    }
+  s += searchInput.value;
+  if (s == "") {
+    alert("There is no content to search...");
+    paragraph.innerHTML = paragraph.textContent;
+    searchInput.value = "";
+    cleartext.innerHTML = ``;
+  } else {
+    changeParagraph(s);
+  }
 }
+
+const changeParagraph = (s) => {
+  s += searchInput.value;
+  paragraph.innerHTML = paragraph.textContent.replace(
+    s,
+    `<span class = "searchedText" id = "searchedText">${s}</span>`
+  );
+  cleartext.innerHTML = `x`;
+};
 
 let a_z = "abcdefghijklmnopqrustuvwxyz!?*,";
 function enter(event) {
-  for (let ln of a_z) {
-    if (event.key == ln) {
-      search(s);
-    }
-  }
   if (event.key == "Backspace") {
     search(s);
+  } else if (event.key == "Enter") {
+    search(s);
+  } else {
+    for (let ln of a_z) {
+      if (event.key == ln) {
+        changeParagraph(s);
+      }
+    }
   }
 }
-
-
